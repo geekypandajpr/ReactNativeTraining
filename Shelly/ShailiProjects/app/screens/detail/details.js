18b75cfdfc6d70b3ab888dce8f290e3ec04b1a70
shaili <shaili.mittal@yusata.com>
2018-08-27 19:23:00 +0530

    latest code.

---
+++ Shelly/ShailiProjects/app/screens/detail/details.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+
+export default class Details extends React.Component
+{
+        render()
+        {
+            return(
+                <View>
+                    <Text>
+                        Hello
+                    </Text>
+                </View>
+            )
+
+        }   
+}
+
+export {Details}
