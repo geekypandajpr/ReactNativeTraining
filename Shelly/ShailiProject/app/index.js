de7344df2cb6d5bf34d958b17f8e35b73698ab33
shaili <shaili.mittal@yusata.com>
2018-08-29 13:38:25 +0530

    Modified

--- Shelly/ShailiProject/app/index.js
+++ Shelly/ShailiProject/app/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { AppRoutes } from './routes/index';
+import {Details} from './screens/detail';
+import {Home} from './screens/home'
 
 export default class Index extends React.Component {
     render() {
         return(
-            <AppRoutes/>
+            <Details/>
         )
     }
 }
--- Shelly/ShailiProject/app/screens/detail/details.js
+++ Shelly/ShailiProject/app/screens/detail/details.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Header} from 'react-native-elements';
+import {View,Text} from 'react-native';
+//import {Header} from 'react-native-elements';
+import {HeaderTab} from '../../components/HeaderTab';
+import styles from './styles';
+import {FotterTab} from '../../components/FotterTab';
+import {  Content, Card, CardItem, Container} from "native-base";
 
 
 
@@ -9,17 +13,78 @@ export default class Details extends React.Component{
 
     render(){
             return(
+               
+                <Container>
+                    
+                    
+               
+                 
+                <View style={styles.parent}>
 
-                  <View>
-                      <Header
-                           placement="left"
-                           leftComponent={{ icon: 'menu', color: '#fff' }}
-                           centerComponent={{ text: 'Details', style: { color: '#fff' } }}
-                              
-                           rightComponent={{ icon: 'search', color: '#fff' }}
+
+                        <HeaderTab
+                                icons1='arrow-bold-left'
+                                type3='Entypo'
+                                onPress={ ()=> this.props.navigation.navigate('Home')}
+                                headerTitle='Shopping'
+                                icons2='search'
+                                type2='FontAwesome'
+                                icons3='menu'
+                                type1='Entypo'
                         />
-                         </View>
+                        <View style={styles.child}></View>
 
+                    <View style={styles.child}>
+                   
+
+                        <View style={styles.child2}>
+                             <Text>hii</Text>
+                        </View>
+
+                        <View style={styles.child2}>
+                             <Text>hii</Text>
+                        </View>
+
+                         <View style={styles.child2}>
+                             <Text>hii</Text>
+                        </View>
+
+
+                     </View>
+
+                    <View style={styles.child}>
+
+                        <View style={styles.child2}>
+                            <Text>hii</Text>
+                        </View>
+
+                         <View style={styles.child2}>
+                            <Text>hii</Text>
+                        </View>
+
+                        <View style={styles.child2}>
+                            <Text>hii</Text>
+                        </View>
+
+                    </View>
+                    <FotterTab
+                     icon1='home'
+                     icon2='camera'
+                     icon3='cart'
+                     icon4='search'
+                     icon5='person'
+                    />
+
+                </View>
+               
+                </Container>
+               
+                
+               
+                
+               
+
+                
             );
         }
     }
