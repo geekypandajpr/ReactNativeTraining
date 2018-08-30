96199aeff3099bf87cf0777d142cdc2e39d18d11
Prem <premsagar.psc@gmail.com>
2018-08-30 19:49:07 +0530

    updated code

--- PremSagar/Assignment1_Survey/app/screens/Result/Styles.js
+++ PremSagar/Assignment1_Survey/app/screens/Result/Styles.js
@@ -4,4 +4,39 @@ export default EStylesheet.create({
     container: {
         flex: 1        
     },
+    resultView: {
+        alignItems: 'center'
+    },
+    cardView: {
+        width: '95%'
+    },
+    cardItem1: {
+        flexDirection: 'row',
+        backgroundColor: '$primaryColor'
+    },
+    cardItem: {
+        flexDirection: 'row'
+    },
+    serialNum: {
+        flex: 0.2
+    },
+    choosedAnsView: {
+        flex: 0.4
+    },
+    correctAnsView: {
+        flex: 0.4
+    },
+    totalScoreView: {
+        flex: 0.5
+    },
+    scoreView: {
+        flex: 0.5
+    },
+    buttonView: {
+        width: '95%'
+    },
+    button: {
+        width: '100%',
+        height: 60
+    }
 });
