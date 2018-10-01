0efc07894644a20b82763794958b03b3c9a0b336
Prem <premsagar.psc@gmail.com>
2018-10-01 09:41:30 +0530

    Commit for Toolbar

---
+++ yusatateam/app/components/Toolbar/Toolbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+    Container,
+    Header,
+    Left,
+    Right,
+    Button,
+    Title,
+    Icon
+} from 'native-base';
+
+export default class Toolbar extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent>
+                            <Icon name='arrow-back' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Header</Title>
+                    </Body>
+                    <Right>
+                        <Button transparent>
+                            <Icon name='menu' />
+                        </Button>
+                    </Right>
+                </Header>
+            </Container>
+        )
+    }
+}
