72e1a5aa3d65c9c14e587864573b113630f493bc
Premsagar\Prem <premsagar.psc@gmail.com>
2018-10-02 18:12:50 +0530

    primary color commit

--- yusatateam/App.js
+++ yusatateam/App.js
@@ -2,18 +2,11 @@ import React from 'react';
 import Index from './app/index';
 import EStyleSheet from 'react-native-extended-stylesheet';
 EStyleSheet.build({
-    // $primaryColor: '#2d4150',
-    // $primaryColorDark: '#041b28',
-    // $primaryColorLight: '#586c7c',
-    // $primaryColortext: '#FFFFFF',
-
     $primaryColor: '#1f667e',
     $primaryColorDark: '#154555',
     $primaryColorLight: '#5494ad',
     $primaryColortext: '#FFFFFF',
-
     $fontSize: '0.8rem',
-
 });
 
 export default class App extends React.Component {
