ea9b39a8ba30929ccb2a17e2f9fe0710ef9b4e3f
vinayak sharma <sharmavinayak17@hotmail.com>
2018-10-03 17:22:53 +0530

    Tab Listing

---
+++ yusatateam/app/screens/Jobs/Jobs.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Tab, Tabs, ScrollableTab } from 'native-base';
+import { View } from 'react-native';
+import { VehicleList } from '../VehicleList/VehicleList';
+import { DeviceSimItem } from '../DeviceSimItem/DeviceSimItem';
+import {ListAccordingToStatus} from './ListAccordingToStatus';
+import {Toolbar} from '../../components'
+import { AppLoading } from 'expo';
+export default class Jobs extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true,
+            tabStatus : 'Activated'
+        },
+        status = ['Activate','Deactivate','Onjob','Completed','Reschedule','Cancelled']
+    }
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+        })
+        this.setState({ isLoading: false })
+    };
+    getStatus = (i,ref,from)=>{
+        this.refs.modal.changeTabStatus(this.status[i])
+
+    };
+
+    render() {
+        return (
+            this.state.isLoading === true ? <AppLoading /> :
+            <View style={{flex:1}}>
+             <Toolbar title='Jobs' leftIcon='arrow-left' leftIconType='Feather' />
+                <Tabs onChangeTab={({ i, ref, from })=> this.getStatus(i,ref,from)} renderTabBar={() => <ScrollableTab />}>
+                    <Tab heading="Entered">
+                        <ListAccordingToStatus ref = "modal"/>
+                    </Tab>
+                    <Tab heading="Accepted">
+                        <ListAccordingToStatus  ref = "modal"/>
+                    </Tab>
+                    <Tab heading="Onjob">
+                        <ListAccordingToStatus  ref = "modal"/>
+                    </Tab>
+                    <Tab heading="Completed">
+                        <ListAccordingToStatus  ref = "modal"/>
+                    </Tab>
+                    <Tab heading="Reschedule">
+                        <ListAccordingToStatus  ref = "modal"/>
+                    </Tab>
+                    <Tab heading="Cancelled">
+                        <ListAccordingToStatus  ref = "modal"/>
+                    </Tab>
+                </Tabs>
+            </View>
+        );
+    }
+}
+
+export { Jobs }
---
+++ yusatateam/app/screens/Jobs/styles.js
@@ -0,0 +1,27 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+export default EStyleSheet.create({
+    container: {
+        flex :1,
+     },
+     flatView : {
+         flex :2, 
+
+     },
+     button : {
+        
+     },
+     viewList : {
+         borderRadius :10,
+     },
+     search : {
+        //flex :0.15,
+        height: 60,
+        backgroundColor: 'gray',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    list : {
+        flex :1
+    }
+    
+})
