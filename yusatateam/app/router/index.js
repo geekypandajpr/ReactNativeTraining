da60e5a04a0e386caa7dab6a356fbb731ca69cda
Prem <premsagar.psc@gmail.com>
2018-10-09 10:17:33 +0530

    no message

--- yusatateam/app/router/index.js
+++ yusatateam/app/router/index.js
@@ -8,49 +8,50 @@ import {
     Jobs
 } from '../screens';
 
-const AppRouter = createStackNavigator(
-    {
-        LogIn: {
-            screen: LogIn,
-            navigationOptions: {
-                header: null
-            }
-        },
-        HomeScreen: {
-            screen: HomeScreen,
-            navigationOptions: {
-                header: null
-            }
-        },
-        Schedule: {
-            screen: Schedule,
-            navigationOptions: {
-                header: null
-            }
-        },
-        VehicleList: {
-            screen: VehicleList,
-            navigationOptions: {
-                header: null
-            }
-        },
-        Sim: {
-            screen: Sim,
-            navigationOptions: {
-                header: null
-            }
-        },
-        Jobs: {
-            screen: Jobs,
-            navigationOptions: {
-                header: null
-            }
+const RouteConfigs = {
+    LogIn: {
+        screen: LogIn,
+        navigationOptions: {
+            header: null
         }
     },
-
-    {
-        initialRouteName: 'LogIn'
+    HomeScreen: {
+        screen: HomeScreen,
+        navigationOptions: {
+            header: null
+        }
+    },
+    Schedule: {
+        screen: Schedule,
+        navigationOptions: {
+            header: null
+        }
+    },
+    VehicleList: {
+        screen: VehicleList,
+        navigationOptions: {
+            header: null
+        }
+    },
+    Sim: {
+        screen: Sim,
+        navigationOptions: {
+            header: null
+        }
+    },
+    Jobs: {
+        screen: Jobs,
+        navigationOptions: {
+            header: null
+        }
     }
-)
+}
+
+const StackNavigatorConfig = {
+    initialRouteName: 'LogIn',
+    headerMode: 'float'
+}
+
+const AppRouter = createStackNavigator(RouteConfigs, StackNavigatorConfig);
 
 export { AppRouter };
