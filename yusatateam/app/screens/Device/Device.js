56985462249e6ab2408246ea9ea8782a30de1778
shaili <shaili.mittal@yusata.com>
2018-10-11 18:13:34 +0530

    no message

--- yusatateam/app/screens/Device/Device.js
+++ yusatateam/app/screens/Device/Device.js
@@ -68,7 +68,7 @@ export default class Device extends React.Component {
 
                                             }}
                                         >
-                                            <FontAwesome name="mobile-phone" size={52} color="#1f667e"
+                                            <FontAwesome name="mobile-phone" size={54} color="#1f667e"
                                              justifyContent="center" alignItems="center" />
                                         </TouchableHighlight>
                                     </View>
