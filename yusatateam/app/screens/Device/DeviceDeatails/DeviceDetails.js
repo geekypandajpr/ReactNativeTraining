718a474fc26633071c322fd4d5d0807c2a7c0946
shaili <shaili.mittal@yusata.com>
2018-10-12 09:55:41 +0530

    no message

---
+++ yusatateam/app/screens/Device/DeviceDeatails/DeviceDetails.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import {
+    View,
+    Modal,
+    Button
+} from 'react-native';
+import { Text } from 'native-base';
+import styles from './styles';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+export default class DeviceDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalVisible: false,
+        }
+    }
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
+    }
+
+    render() {
+        return (
+            <View>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        this.setModalVisible(!this.state.modalVisible);
+                    }}>
+                    <View style={styles.container}>
+
+                        <View style={styles.header_view}>
+
+                            <View style={styles.service_num}>
+                                <Text style={styles.header_text}>Item12</Text>
+                            </View>
+                            <View style={styles.schedule_view}>
+                                <View style={[styles.Status_Button, { backgroundColor: "#5cb85c" }]} >
+                                    <Text style={styles.Status_Style}>Tested Ok</Text>
+                                </View>
+                            </View>
+                        </View>
+
+                        <View style={styles.View_Container}>
+
+                            <View style={{ flexDirection: 'row' }}>
+                                <View style={{ flex: 1, flexDirection: 'row' }}>
+                                    <View >
+                                        <Text style={styles.Order_text}>OrderNumber :</Text>
+                                    </View>
+                                    <View style={{ marginLeft: 10 }}>
+                                        <Text style={styles.Order_texts}>ORDER001</Text>
+                                    </View>
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginTop: 3 }}>
+                                <View style={styles.Level_Flex}>
+                                    <Text style={styles.Text_Style}>Imei </Text>
+                                </View>
+                                <View style={styles.Column_Flex}>
+                                    <Text>:</Text>
+                                </View>
+                                <View style={styles.Text_Flex}>
+                                    <Text style={styles.View_Style}>imei252</Text>
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginTop: 3 }}>
+                                <View style={styles.Level_Flex} >
+                                    <Text style={styles.Text_Style}>Esn</Text>
+                                </View>
+                                <View style={styles.Column_Flex}>
+                                    <Text>:</Text>
+                                </View>
+                                <View style={styles.Text_Flex}>
+                                    <Text style={styles.View_Style}>Esn1256</Text>
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginTop: 3 }}>
+                                <View style={styles.Level_Flex} >
+                                    <Text style={styles.Text_Style}>Price </Text>
+                                </View>
+                                <View style={styles.Column_Flex}>
+                                    <Text>:</Text>
+                                </View>
+                                {/* <View style={{ flex: 2, justifyContent: 'flex-start', marginTop: 4 }}>
+                                    <FontAwesome name='rupee' size={20} color='gray' />
+                                </View> */}
+                                <View style={styles.Text_Flex}>
+                                    <Text style={styles.View_Style}>1520</Text>
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginTop: 3 }}>
+                                <View style={styles.Level_Flex} >
+                                    <Text style={styles.Text_Style}>HarnessType</Text>
+                                </View>
+                                <View style={styles.Column_Flex}>
+                                    <Text>:</Text>
+                                </View>
+                                <View style={styles.Text_Flex}>
+                                    <Text style={styles.View_Style}>harnessTest</Text>
+                                </View>
+                            </View>
+
+                            <View style={{ flexDirection: 'row', marginTop: 3 }}>
+                                <View style={styles.Level_Flex} >
+                                    <Text style={styles.Text_Style}>Peripheral</Text>
+                                </View>
+                                <View style={styles.Column_Flex}>
+                                    <Text>:</Text>
+                                </View>
+                                <View style={styles.Text_Flex}>
+                                    <Text style={styles.View_Style}>peripheralTest</Text>
+                                </View>
+                            </View>
+                            
+                            <View style={{ flexDirection: 'row', marginTop: 3 }}>
+                                <View style={styles.Level_Flex} >
+                                    <Text style={styles.Text_Style}>Model</Text>
+                                </View>
+                                <View style={styles.Column_Flex}>
+                                    <Text>:</Text>
+                                </View>
+                                <View style={styles.Text_Flex}>
+                                    <Text style={styles.View_Style}>ModelTest</Text>
+                                </View>
+                            </View>
+
+                             <View style={{ flexDirection: 'row', marginTop: 3 }}>
+                                <View style={styles.Level_Flex} >
+                                    <Text style={styles.Text_Style}>Manufacturer</Text>
+                                </View>
+                                <View style={styles.Column_Flex}>
+                                    <Text>:</Text>
+                                </View>
+                                <View style={styles.Text_Flex}>
+                                    <Text style={styles.View_Style}>HP</Text>
+                                </View>
+                            </View>
+
+
+                            <View style={{ marginTop: 6 }}>
+                                <View >
+                                    <Text style={styles.Text_Style}>Item Description - </Text>
+                                </View>
+                                <View >
+                                    <Text style={styles.ViewDescription_Text} >This is device,used for purpose of installation
+                                    in the vehicle device install after test </Text>
+                                </View>
+                            </View>
+                            <View style={styles.Button_View}>
+                                <View style={{ flex: 1 }}></View>
+                                <View style={styles.Button_Style}>
+                                    <Button
+                                        onPress={() => {
+                                            this.setModalVisible(!this.state.modalVisible);
+                                        }}
+                                        title="Close"
+                                        color="#0073b7"
+
+                                    />
+                                </View>
+                            </View>
+
+                        </View>
+                    </View>
+                </Modal>
+            </View>
+
+        );
+    }
+}
+export { DeviceDetails }
---
+++ yusatateam/app/screens/Device/DeviceDeatails/styles.js
@@ -0,0 +1,156 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+
+    container:
+    {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        backgroundColor: '#00000090',
+        flexDirection: 'column',
+    },
+    header_view: {
+        width: '100%',
+        height: 50,
+        backgroundColor: '#EEEEF0',
+        justifyContent: 'center',
+        elevation: 5,
+        flexDirection: 'row'
+    },
+    header_text: {
+
+        color: '#000',
+        fontSize: '1.1rem',
+        fontWeight: 'bold',
+        margin: 15
+    },
+    Order_text:
+    {
+        fontStyle: 'normal',
+        fontSize: '1.2rem',
+        color: '#000',
+        fontWeight: '100',
+    },
+    Order_texts:
+    {
+        fontStyle: 'normal',
+        fontSize: '1.2rem',
+        color: 'gray',
+        fontWeight: '100',
+    },
+    Text_Style:
+    {
+        fontStyle: 'normal',
+        fontSize: '1.1rem',
+        color: '#000',
+    },
+    View_Style:
+    {
+        fontStyle: 'normal',
+        fontSize: '1.1rem',
+        color: 'gray',
+        marginLeft: 11
+    },
+    ViewDescription_Text:
+    {
+        fontStyle: 'normal',
+        fontSize: '1.1rem',
+        color: 'gray',
+
+    },
+    service_num: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    job_text: {
+        fontSize: '0.8rem',
+        fontWeight: 'bold',
+        color: '#000',
+        margin: 5
+    },
+    schedule_view: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 140
+    },
+    View_Container:
+    {
+        backgroundColor: '#FFFFFF',
+        height: '65%',
+        width: '100%',
+        paddingTop: 15,
+        paddingLeft: 15,
+        paddingRight: 15
+    },
+    Provider_View:
+    {
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'gray',
+        width: 70,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 90
+    },
+    providerStyle:
+    {
+        fontStyle: 'normal',
+        fontSize: '1rem',
+        fontWeight: 'bold',
+        color: '#0073b7',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    Status_Button:
+    {
+        borderRadius: 10,
+        borderWidth: 3,
+        width: 100,
+        height: 35,
+        borderColor: '#EEEEF0',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    Status_Style:
+    {
+        fontStyle: 'normal',
+        fontSize: '1rem',
+        fontWeight: 'bold',
+        color: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    Level_Flex:
+    {
+        flex: 1.2
+    },
+    Column_Flex:
+    {
+        flex: 0.1
+    },
+    Text_Flex:
+    {
+        flex: 2,
+        justifyContent: 'flex-start'
+    },
+    Mobile_Level: {
+        flexDirection: 'row',
+        marginTop: 6
+    },
+    Button_View:
+    {
+        flex: 1,
+        flexDirection: 'row',
+        marginTop: 6
+    },
+    Button_Style:
+    {
+        flex: 1,
+        width: 30,
+        marginLeft: 100
+    }
+
+})
