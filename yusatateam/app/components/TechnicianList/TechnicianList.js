fcbb02c61c8c74972ca77946e3f92182c181edc7
shaili <shaili.mittal@yusata.com>
2018-11-02 16:36:24 +0530

    no message

--- yusatateam/app/components/TechnicianList/TechnicianList.js
+++ yusatateam/app/components/TechnicianList/TechnicianList.js
@@ -1,26 +1,149 @@
 import React from 'react';
 import {
-    View, TouchableHighlight
+    View,
+    Text,
+    Button,
+    FlatList
 } from 'react-native';
-import { Card, Text } from 'native-base';
-//import { Text, Icon } from 'native-base';
+import { Ionicons, Entypo } from '@expo/vector-icons';
+import { Card, } from 'native-base';
+
+import { AppLoading } from 'expo';
 import styles from './styles';
+import { Toolbar } from '../../components';
+import TechnicianData from '../../assets/JSONData/TechnicianData'
 
 export default class TechnicianList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoading: true,
+
+        };
+        this.modalRef = React.createRef();
+    }
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+        })
+        this.setState({ isLoading: false })
+    }
     render() {
+        // const { navigate } = this.props.navigation;
+        // const { goBack } = this.props.navigation;
         return (
-            <View style={{ flex: 1, backgroundColor: 'gray' }}>
-                <Card style={styles.MainCard}>
-                    <View style={{ flex: 1,backgroundColor:'red',justifyContent:'center',alignItems:'center' }}>
-                    <View style={styles.Profile_View}></View>
-                    </View>
-                    <View style={{ flex: 3,backgroundColor:'green'}}></View>
-                </Card>
-                <Card style={styles.MainCard}>
-                    <View style={{ flex: 1 }}></View>
-                    <View style={{ flex: 1 }}></View>
-                </Card>
-            </View>
+
+            this.state.isLoading === true ? <AppLoading /> :
+                <View style={styles.container}>
+                    {/* <Toolbar title='Sim' leftIcon='arrow-left' leftIconType='Feather' onLeftButtonPress={() => goBack()}
+                        setting='md-settings' settingType='Ionicons' onSettingsPress={() => navigate('Settings')} /> */}
+
+                    <FlatList
+                        data={TechnicianData}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item, index }) =>
+
+
+                            <Card style={styles.MainCard}>
+                                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                                    <View style={styles.Profile_View}></View>
+                                </View>
+
+                                <View style={{ flex: 3, marginTop: 5 }}>
+
+                                    <View style={{flex:1,flexDirection:'row'}}>
+                                        <View style={{flex:1,justifyContent:'center'}}>
+                                            <Text style={styles.Text_Style}>{item.Name}</Text>
+                                        </View>
+                                        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+                                            <Ionicons name='ios-add-circle-outline' size={27}></Ionicons>
+                                        </View>
+                                    </View>
+
+                                  <View style={{ flex: 1, flexDirection: 'row' }}>
+                                        <View style={{ flex: 0.2, justifyContent: 'center' }}>
+                                            <Ionicons name='ios-call' size={27} color='#5cb85c' />
+                                        </View>
+                                        <View style={{ flex: 2,justifyContent:'center' }}>
+                                            <Text style={styles.Call_Style}>9829876456</Text>
+                                        </View>
+                                    </View>
+
+                                    <View style={{ flex: 1, flexDirection: 'row' }}>
+                                        <View style={{ flex: 1.2, justifyContent: 'center' }}>
+                                            <Text style={styles.Text_Style}>Completed Job</Text>
+                                        </View>
+                                        <View style={{ flex: 0.1, justifyContent: 'center' }}>
+                                            <Text>:</Text>
+                                        </View>
+                                        <View style={{ flex: 1.2, justifyContent: 'center', marginLeft: 5 }}>
+                                            <Text style={styles.View_Style}>{item.Completed}</Text>
+                                        </View>
+                                    </View>
+
+                                    
+                                    <View style={{ flex: 1, flexDirection: 'row' }}>
+                                        <View style={{ flex: 1.2, justifyContent: 'center' }}>
+                                            <Text style={styles.Text_Style}>Total Job</Text>
+                                        </View>
+                                        <View style={{ flex: 0.1, justifyContent: 'center' }}>
+                                            <Text>:</Text>
+                                        </View>
+                                        <View style={{ flex: 1.2, justifyContent: 'center', marginLeft: 5 }}>
+                                            <Text style={styles.View_Style}>{item.Completed}</Text>
+                                        </View>
+                                    </View>
+
+                                     <View style={{ flex: 1, flexDirection: 'row' }}> 
+                                        <View style={{ flex: 0.2, justifyContent: 'center' }}>
+                                            <Entypo name='location-pin' size={23} color='red' />
+                                        </View>
+                                        <View style={{ flex: 2, justifyContent: 'center' }}>
+                                            <Text style={styles.call_Style}>{item.Status}</Text>
+                                        </View>
+                                    </View>
+
+                                    {/* <View style={{ flex: 1, flexDirection: 'row' }}>
+                                        <View style={{ flex: 1.2, justifyContent: 'center' }}>
+                                            <Text style={styles.Text_Style}>Completed </Text>
+                                        </View>
+                                        <View style={{ flex: 0.1, justifyContent: 'center' }}>
+                                            <Text>:</Text>
+                                        </View>
+                                        <View style={{ flex: 2, justifyContent: 'center', marginLeft: 5 }}>
+                                            <Text style={styles.View_Style}>{item.Completed}</Text>
+                                        </View>
+                                    </View> */}
+
+                                    
+
+                                    {/*
+                                        {/* <View style={{ flex: 1, marginBottom: 2 }}> */}
+
+                                            {/* */}
+                                            {/* <View style={{ borderRadius: 7, borderWidth: 1, borderColor: 'white' }}>
+                                                <Button style={{ height: 20, }}
+                                                    onPress={() => {
+                                                        alert
+                                                    }}
+                                                    title="Add"
+                                                    color="#0073b7"
+
+                                                />
+                                            </View> */}
+                                      
+                                    
+
+                                </View>
+                            </Card>
+                        }></FlatList>
+
+
+                </View>
+
 
         )
     }
--- yusatateam/app/components/TechnicianList/styles.js
+++ yusatateam/app/components/TechnicianList/styles.js
@@ -1,22 +1,54 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 export default EStyleSheet.create({
-
+    container:
+    {
+        flex: 1,
+        backgroundColor: '#D3D3D3',
+       
+    },
     MainCard: {
         flex: 1,
         flexDirection: 'row',
-        borderRadius: 10,
-        borderWidth: 5,
+        borderRadius: 5,
+        borderWidth: 3,
         borderColor: 'white',
-        //backgroundColor:'green'
+    
     },
-
+    viewStyle:
+    {
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingTop: 5,
+        flex: 1,
+    },
     Profile_View: {
         justifyContent: 'center',
         alignItems: 'center',
-        height: 54,
-        width: 54,
-        borderRadius: 27,
+        height: 80,
+        width: 80,
+        borderRadius: 40,
         borderWidth: 1,
         borderColor: 'gray'
-    }
+    },
+    Text_Style:
+    {
+        fontStyle: 'normal',
+        fontSize: '1rem',
+        color: '#000'
+    },
+    View_Style:
+    {
+        fontStyle: 'normal',
+        fontSize: '0.9rem',
+        color: 'gray',
+        marginLeft: 10
+    },
+
+    Call_Style:
+    {
+        fontStyle: 'normal',
+        fontSize: '0.9rem',
+        color: 'gray',
+        
+    },
 });
