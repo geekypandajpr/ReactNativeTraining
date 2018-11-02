1cdc92d912bcc9536fddff88cd50feeaffdbada6
Prem <premsagar.psc@gmail.com>
2018-11-02 14:13:38 +0530

    no message

--- yusatateam/app/screens/ForgotPassword/ForgotPassword.js
+++ yusatateam/app/screens/ForgotPassword/ForgotPassword.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {
-    View,
-    Image,
-    ImageBackground,
-    ScrollView,
-    TouchableWithoutFeedback
-} from 'react-native';
+import { View} from 'react-native';
 import styles from './Styles';
 import { AppLoading } from 'expo';
 import {
-    CheckBox,
     Button,
-    Text,
-    Toast
+    Item,
+    Input,
+    Label,
+    Text
 } from 'native-base';
-import { Statusbar } from '../../components';
+import { Statusbar, InputWithIcon } from '../../components';
+import { Feather } from '@expo/vector-icons';
+import colors from '../../constants/colors';
 
 export default class ForgotPassword extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isLoading: true
+            isLoading: true,
+            isButtonEnabled: true,
+            email: ''
         }
+        this.onEmailChange = this.onEmailChange.bind(this);
     }
 
     async componentWillMount() {
@@ -33,13 +33,51 @@ export default class ForgotPassword extends React.Component {
         this.setState({ isLoading: false });
     }
 
+    onEmailChange(text) {
+        this.setState({ email: text })
+    }
+
+    doSubmit() {
+
+    }
+
     render() {
         const { goBack } = this.props.navigation;
         return (
             this.state.isLoading === true ? <AppLoading /> :
 
-            <View style={styles.container}>
-                <Statusbar backgroundColor={'transparent'} barStyle="light-content" />
+            <View style={{ flex: 1 }}>
+                <Statusbar backgroundColor={colors.STATUSBAR_COLOR} barStyle="light-content" />
+                
+                <View style={styles.container}>
+                    <View style={styles.appBar}>
+                        <Feather name='arrow-left' color="#000" size={24} onPress={() => goBack()}/>
+                    </View>
+                    <View style={styles.forgot_view}>
+                        <Text style={styles.forgot_text}>Forgot Password ?</Text>
+                    </View>
+                    <View style={styles.input_view}>
+                        <Item floatingLabel>
+                            <Label style={{color:'gray', fontSize: 15}}>Email</Label>
+                            <Input
+                                style={{color:'#000', fontSize: 15}}
+                                value={this.state.email}
+                                keyboardType={'email-address'}
+                                returnKeyType={'go'}
+                                onChangeText={(text) => this.onEmailChange(text)}
+                                secureTextEntry={false}
+                                onSubmitEditing={this.doSubmit}
+                            />
+                        </Item>
+                    </View>
+                    <View style={styles.button_view}>
+                        <Button disabled={this.state.isButtonEnabled}
+                            style={styles.button}
+                            onPress={this.doSubmit} >
+                            <Text>Submit</Text>
+                        </Button>
+                    </View>
+                </View>
             </View>
         )
     }
--- yusatateam/app/screens/ForgotPassword/Styles.js
+++ yusatateam/app/screens/ForgotPassword/Styles.js
@@ -1,9 +1,52 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { Platform, StatusBar, StyleSheet } from 'react-native';
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
 
 export default EStyleSheet.create({
     container: {
         flex:1,
         alignItems: 'center',
-        backgroundColor:'red'
+        backgroundColor: '#fff'
+    },
+    appBar: {
+        //backgroundColor: '#171c2e',
+        backgroundColor: 'white',
+        height: APPBAR_HEIGHT,
+        width: '90%',
+        //elevation: 10,
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    forgot_view: {
+        height: 40,
+        width: '90%'
+    },
+    forgot_text: {
+        fontSize: '1.3rem',
+        color: '#171c2e',
+        fontWeight: '500'
+    },
+    input_view: {
+        width: '90%'
+    },
+    input: {
+        fontSize: '0.9rem'
+    },
+    email_text: {
+        fontSize: '1rem',
+        color: '#000'
+    },
+    button_view: {
+        width: '90%',
+        height: 40,
+        marginTop: 20
+    },
+    button: {
+        // backgroundColor: '#5cb85c',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
