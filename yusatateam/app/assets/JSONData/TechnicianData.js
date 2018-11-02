5494cb3e9eb679300d4a10ef3656f14569c376d3
shaili <shaili.mittal@yusata.com>
2018-11-02 16:41:11 +0530

    no message

---
+++ yusatateam/app/assets/JSONData/TechnicianData.js
@@ -0,0 +1,54 @@
+const Tdata= [
+    {
+       Name :'Shjja Mittal',
+       Completed:'5',
+       Status:'OnJob'
+    },
+    {
+        Name :'Efjk Hkwrf',
+        Completed:'9',
+        Status:'OnWay'
+     },
+     {
+        Name :'Hdsj Mfuew',
+        Completed:'7',
+        Status:'Absent'
+     },
+     {
+        Name :'Shjja Jlefb',
+        Completed:'5',
+        Status:'OnJob'
+     },
+     {
+        Name :'Suwe Gwllej',
+        Completed:'8',
+        Status:'Absent'
+     },
+     {
+        Name :'shjja',
+        Completed:'5',
+        Status:'OnWay'
+     },
+     {
+        Name :'shjja',
+        Completed:'5',
+        Status:'OnJob'
+     },
+     {
+        Name :'hdsj',
+        Completed:'7',
+        Status:'Absent'
+     },
+     {
+        Name :'shjja',
+        Completed:'5',
+        Status:'OnWay'
+     },
+     {
+        Name :'suwe',
+        Completed:'8',
+        Status:'OnWay'
+     },
+];
+
+module.exports = Tdata;
