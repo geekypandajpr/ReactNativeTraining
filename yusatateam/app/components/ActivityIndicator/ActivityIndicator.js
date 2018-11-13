8e2e0f09593c1db79e36f782f29de9b27ccca336
shaili <shaili.mittal@yusata.com>
2018-11-13 16:14:17 +0530

    no message

---
+++ yusatateam/app/components/ActivityIndicator/ActivityIndicator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { 
+    View,
+    ActivityIndicator
+    } from 'react-native';
+import Styles from './Styles';
+export default class Activityindicator extends React.Component
+{
+    render()
+    {
+        return
+        (
+        <View style={{flex:1}}>
+        
+        </View>
+        );
+    }
+
+}
+export{ Activityindicator }
---
+++ yusatateam/app/components/ActivityIndicator/index.js
@@ -0,0 +1,1 @@
+export { Activityindicator } from './ActivityIndicator';
