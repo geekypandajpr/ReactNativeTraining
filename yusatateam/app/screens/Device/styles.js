34f38bb41a5499df6266b697540ccbf62b5cecfd
shaili <shaili.mittal@yusata.com>
2018-11-15 10:20:03 +0530

    no message

--- yusatateam/app/screens/Device/styles.js
+++ yusatateam/app/screens/Device/styles.js
@@ -1,104 +1,47 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
-    container:
-    {
+    container:{
         flex: 1,
-        backgroundColor: '#D3D3D3',
+        backgroundColor: 'white',
     },
-    viewStyle:
-    {
-        paddingLeft: 5,
-        paddingRight: 5,
-        paddingTop: 5,
+    viewStyle:{
+        paddingLeft: 4,
+        paddingRight: 4,
+        paddingTop: 4,
         flex: 1,
     },
-    mainCard:
-    {
+    mainCard:{
         flex: 1,
         flexDirection: 'row',
-        borderRadius: 5,
-        borderWidth: 3,
+        // borderRadius: 5,
+        // borderWidth: 3,
         borderColor: 'white'
     },
-    First_View:
-    {
+    First_View:{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     },
-    Second_View:
-    {
+    Second_View:{
         flex: 4,
     },
-    Level_Second:
-    {
-        flex: 1,
-        justifyContent: 'flex-start'
-    },
-    Level_Row:
-    {
-        flex: 1,
-        flexDirection: 'row'
-    },
-    Margin_Row:
-    {
+    Margin_Row:{
         flex: 1,
         flexDirection: 'row',
-        marginTop: 5
+        marginTop: 2
     },
-    Level_Head:
-    {
-        flex:0.7,
+    Level_Second:{
+        flex: 1,
         justifyContent: 'flex-start'
     },
-    Header_Style:
-    {
+    Header_Style:{
         fontStyle: 'normal',
         fontSize: '1rem',
         fontWeight: '500',
         color: '#000'
     },
-    Text_Style:
-    {
-        fontStyle: 'normal',
-        fontSize: '0.9rem',
-        color: '#000'
-    },
-    View_Style:
-    {
-        fontStyle: 'normal',
-        fontSize: '0.9rem',
-        color: 'gray',
-        marginLeft: 10
-    },
-    modal_style:
-    {
-        fontStyle: 'normal',
-        fontSize: '0.9rem',
-        color: 'gray',
-    },
-    providerStyle:
-    {
-        fontStyle: 'normal',
-        fontSize: '1rem',
-        fontWeight: 'bold',
-        color: '#1f667e',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    Provider_View:
-    {
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: 'gray',
-        width: 70,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 11
-    },
-    Status_Button:
-    {
+    Status_Button:{
         borderRadius: 15,
         borderWidth: 3,
         width: 110,
@@ -107,8 +50,7 @@ export default EStyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    Status_Style:
-    {
+    Status_Style:{
         fontStyle: 'normal',
         fontSize: '1rem',
         fontWeight: 'bold',
@@ -116,11 +58,51 @@ export default EStyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    Level_Style:
-    {
+    Level_Row:{
+        flex: 1,
+        flexDirection: 'row'
+    },
+    Level_Head:{
+        flex:0.7,
+        justifyContent: 'flex-start'
+    },
+    Text_Style:{
+        fontStyle: 'normal',
+        fontSize: '0.9rem',
+        color: '#000'
+    },
+    Level_Style:{
         flex: 2,
         justifyContent: 'flex-start'
-    }
+    },
+    View_Style:{
+        fontStyle: 'normal',
+        fontSize: '0.9rem',
+        color: 'gray',
+        marginLeft: 10
+    },
+    modal_style:{
+        fontStyle: 'normal',
+        fontSize: '0.9rem',
+        color: 'gray',
+    },
+    Provider_View:{
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'gray',
+        width: 70,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 11
+    },
+    providerStyle:{
+        fontStyle: 'normal',
+        fontSize: '1rem',
+        fontWeight: 'bold',
+        color: '#1f667e',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
 
 })
