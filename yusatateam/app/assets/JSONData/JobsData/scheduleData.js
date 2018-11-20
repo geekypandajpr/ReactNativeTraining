a53ad1a4f01425b01a78acde8d49a7c1b191703c
vinayak sharma <sharmavinayak17@hotmail.com>
2018-11-20 13:35:02 +0530

    code

--- yusatateam/app/assets/JSONData/JobsData/scheduleData.js
+++ yusatateam/app/assets/JSONData/JobsData/scheduleData.js
@@ -1,17 +1,18 @@
 const ScheduleData= [
     {
-        'jobNumber': 'VOCT092015',
-        'jobStatus': 'Schedule',
+        'jobNumber': 'JOBS20NOV2018',
+        'jobStatus': 'completed',
         'companyName': 'Yusata infotech Private Limited',
         'jobType': 'Install',
         'jobName': 'Job Name',
-        'scheduleDate': '10/10/2018 20:00',
+        'scheduleDate': '10 Nov 2018 20:00',
         'servicePerson': 'Yash Gulati',
         'contactPerson': 'Mr. Vinayak Sharma',
         'contactNumber': '+91 8945623622',
         'cashOnDelivery': 'Yes',
         'amount': 'Rs. 5000',
-        'training': 'Yes'
+        'training': 'Yes',
+        'location': '84/122 sector 8, pratap nagar, jaipur rajasthan'
     },
     {
         'jobNumber': 'VOCT092016',
