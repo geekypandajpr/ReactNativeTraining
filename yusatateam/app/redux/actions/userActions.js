0f66a491f19232782edc4dcba9e13545bc6a5b45
Prem <premsagar.psc@gmail.com>
2018-11-21 18:25:20 +0530

    no message

--- yusatateam/app/redux/actions/userActions.js
+++ yusatateam/app/redux/actions/userActions.js
@@ -1,7 +1,7 @@
 import { USER } from '../common/actionTypes';
 // export const loginRequest = (loginCredentials) => { type: USER.FETCHING_DATA, loginCredentials }
 
-export function loginRequest(loginCredentials) {
+export const loginRequest = (loginCredentials) => {
     return {
         type: USER.FETCHING_DATA,
         loginCredentials
