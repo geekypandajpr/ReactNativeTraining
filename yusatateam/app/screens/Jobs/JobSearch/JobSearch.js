40065711193c3c8e94fed84c2163b8bc1e7c8da4
vinayak sharma <sharmavinayak17@hotmail.com>
2018-11-23 17:09:05 +0530

    Search Checkbox

--- yusatateam/app/screens/Jobs/JobSearch/JobSearch.js
+++ yusatateam/app/screens/Jobs/JobSearch/JobSearch.js
@@ -6,13 +6,16 @@ import {
     TextInput,
     Picker,
     ScrollView,
-    FlatList
+    FlatList,
+    CheckBox
 } from 'react-native';
-import { List, ListItem, CheckBox, Body } from 'native-base';
+import { List, ListItem, Body, Button ,} from 'native-base';
 import styles from './styles';
-import { Checkbox } from '../../../components';
 import { TouchableWithoutFeedback } from 'react-native';
 import { Toolbar, JobsComponent, SearchBar } from '../../../components';
+import { Checkbox } from '../../../components';
+
+var map1 = new Map();
 export default class JobSearch extends React.Component {
     constructor(props) {
         super(props);
@@ -25,12 +28,17 @@ export default class JobSearch extends React.Component {
             sim: '',
             status: '',
             data: this.props.navigation.state.params.item,
-            dropdownbool: false
+            dropdownbool: false,
+            checkboxes: [],
+            plans: {},
+            checkbox: false
         }
         this.arrayholder = [];
+        
     }
     componentDidMount() {
         this.arrayholder = this.state.data;
+
     }
     async componentWillMount() {
         await Expo.Font.loadAsync({
@@ -41,23 +49,20 @@ export default class JobSearch extends React.Component {
         this.setState({ isLoading: false })
     }
     SearchFilterFunction(text) {
-        var searchField;
-        //console.log(this.state.status);
-        if(this.state.status== 'jobNumber')
-        {
-          const newData = this.arrayholder.filter(function (item) {
-            const itemData = item.jobNumber.toUpperCase()
-            const textData = text.toUpperCase()
-            return itemData.indexOf(textData) > -1
-        })
-        this.setState({
-            data: newData,
-            text: text
-        },
-        )
+        if (this.state.status == 'jobNumber') {
+            const newData = this.arrayholder.filter(function (item) {
+                const itemData = item.jobNumber.toUpperCase()
+                const textData = text.toUpperCase()
+
+                return itemData.indexOf(textData) > 0
+            })
+            this.setState({
+                data: newData,
+                text: text
+            },
+            )
         }
-        if(this.state.status== 'scheduleDate')
-        {
+        if (this.state.status == 'scheduleDate') {
             const newData = this.arrayholder.filter(function (item) {
                 const itemData = item.scheduleDate.toUpperCase()
                 const textData = text.toUpperCase()
@@ -69,11 +74,12 @@ export default class JobSearch extends React.Component {
             },
             )
         }
-        if(this.state.status== 'jobType')
-        {
+        if (this.state.status == 'jobType') {
             const newData = this.arrayholder.filter(function (item) {
                 const itemData = item.jobType.toUpperCase()
                 const textData = text.toUpperCase()
+                console.log(textData);
+                console.log(itemData.indexOf(textData));
                 return itemData.indexOf(textData) > -1
             })
             this.setState({
@@ -81,21 +87,36 @@ export default class JobSearch extends React.Component {
                 text: text
             },
             )
-        }    
-    }
-    changeCheckboxStatus () 
-    {
-        if(this.state.data.checkStatus == false)
-        {
-            this.setState({checkbox : true})
-        }
-        else{
-            this.setState({checkbox : false})
         }
     }
 
+
+    toggleCheckbox(id) {
+
+        let checkboxes = this.state.checkboxes;
+        if (checkboxes.includes(id)) {
+            const index = checkboxes.indexOf(id);
+            checkboxes.splice(index, 1);
+            map1.delete(id);
+        } else {
+            checkboxes = checkboxes.concat(id);
+            map1.set(id, true);
+        }
+
+        this.setState({ checkboxes });
+    }
+
+
     render() {
+
         const { goBack } = this.props.navigation;
+        const checkboxes = this.state.checkboxes;
+        console.log("MS CB1: " + checkboxes);
+
+
+        console.log(map1);
+
+
         return (
             <View style={styles.container}>
                 <Toolbar title='Jobs'
@@ -106,23 +127,27 @@ export default class JobSearch extends React.Component {
                     onChangeText={(text) => this.SearchFilterFunction(text)} />
 
                 {
-                    this.state.dropdownbool == false ? null : 
-                    <Picker selectedValue={this.state.status} onValueChange={(itemValue) => this.setState({ status: itemValue })}>
-                        <Picker.Item label="Job Name" value="jobNumber" />
-                        <Picker.Item label="Schedule Date" value="scheduleDate" />
-                        <Picker.Item label="Job Type" value="jobType" />
-                    </Picker>
+                    this.state.dropdownbool == false ? null :
+                        <Picker selectedValue={this.state.status} onValueChange={(itemValue) => this.setState({ status: itemValue })}>
+                            <Picker.Item label="Job Name" value="jobNumber" />
+                            <Picker.Item label="Schedule Date" value="scheduleDate" />
+                            <Picker.Item label="Job Type" value="jobType" />
+                        </Picker>
                 }
 
                 <FlatList
                     data={this.state.data}
-                    keyExtractor={(item, index) => index.toString()}
+                    keyExtractor={(item, index) => item.jobNumber}
                     renderItem={({ item, index }) =>
+
                         <List style={styles.list}>
                             <ListItem icon style={styles.listitem}>
-                                <Checkbox checked={item.checkStatus}
-                                onPress={() => {this.changeCheckboxStatus}}
+                            <CheckBox
+                                    value={map1.get(item.jobNumber)}
+                                    onChange={() => this.toggleCheckbox(item.jobNumber)}
                                 />
+
+                                <Text>{map1.get(item.jobNumber)}</Text>
                                 <Body style={styles.body}>
                                     <TouchableWithoutFeedback>
                                         <View>
