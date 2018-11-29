77dcfdda98636fe1a27cbf931d1acce80b759aa1
Prem <premsagar.psc@gmail.com>
2018-11-29 11:17:07 +0530

    commit for header searchbar

--- yusatateam/app/screens/Jobs/jobAssign/jobAssign.js
+++ yusatateam/app/screens/Jobs/jobAssign/jobAssign.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import {
-    Text,
-    View,
-    FlatList,
-    TouchableOpacity,
-    Alert,
-    Modal
-} from 'react-native';
-import {Card} from 'native-base';
+import { View, FlatList, Alert,  Modal } from 'react-native';
+import { AppLoading } from 'expo';
+import { Header, Body, Item, Button, Input, Card } from 'native-base';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
+
 import styles from './styles';
-import { TouchableWithoutFeedback } from 'react-native';
-import { Toolbar, SearchBar,TechnicianList } from '../../../components';
+import { TechnicianList, SearchHeader } from '../../../components';
 import techDatas from '../../../assets/JSONData/TechnicianData';
+
 
 
 export default class JobAssign extends React.Component {
@@ -21,27 +17,57 @@ export default class JobAssign extends React.Component {
             modalVisible: false,
             isLoading: true,
             data: techDatas,
+            searchValue: ''
         }  
         this.arrayholder = [];
+        this.setModalVisible = this.setModalVisible.bind(this);
+        this.onSearchTextChange = this.onSearchTextChange.bind(this);
+        this.onClose = this.onClose.bind(this);
+        this.searchText = this.searchText.bind(this);
+        this.onClear = this.onClear.bind(this);
+    }
+
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+        })
+        this.setState({ isLoading: false })
     }
 
     componentDidMount() {
         this.arrayholder = this.state.data;
     }
+
     setModalVisible(visible) {
         this.setState({ modalVisible: visible});
     }
-    SearchFilterFunction(text) {
-            const newData = this.arrayholder.filter(function (item) {
-                const itemData = item.name.toUpperCase()
-                const textData = text.toUpperCase()
-                return itemData.indexOf(textData) > -1
-            })
-            this.setState({
-                data: newData,
-                text: text
-            },
-            )
+
+    searchText(text) {
+        const newData = this.arrayholder.filter(function (item) {
+            const itemData = item.name.toUpperCase()
+            const textData = text.toUpperCase()
+            return itemData.indexOf(textData) > -1
+        });
+        this.setState({
+            data: newData,
+            text: text
+        });
+    }
+
+    onSearchTextChange(searchValue) {
+        this.setState({searchValue}, function() {
+            this.searchText(searchValue);
+        });
+    }
+
+    onClear() {
+        this.onSearchTextChange('');
+    }
+
+    onClose() {
+        this.setState({ modalVisible: false });
     }
 
     alertBoxMessage()
@@ -56,13 +82,14 @@ export default class JobAssign extends React.Component {
             { cancelable: false }
           );
     }
-    render() {
 
+    render() {
         return (
-            <View style={styles.container}>
+            this.state.isLoading === true ? <AppLoading /> :
+            <View>
                 <Modal
                     animationType="slide"
-                    transparent={true}
+                    transparent={false}
                     visible={this.state.modalVisible}
                     onDismiss={() => {
                         this.setState({ modalVisible: !this.state.modalVisible })
@@ -70,22 +97,28 @@ export default class JobAssign extends React.Component {
                     onRequestClose={() => {
                         this.setState({ modalVisible: !this.state.modalVisible })
                     }}>
-                    <View style={{ flex: 1, backgroundColor: 'white' }}>
-                    <View style={{ height: '100%', width: '100%', position: 'absolute', bottom: 0 }}>
-                <SearchBar placeholder={'Search jobs'}
-                    onChangeText={(text) => this.SearchFilterFunction(text)} />
-                         <FlatList
-                            data={this.state.data}
-                            keyExtractor={(item, index) => index.toString()}
-                            renderItem={({ item, index }) =>
-                                <TechnicianList
-                                    data={item}
-                                    
-                                    assignJobs={this.alertBoxMessage}
-                                />
-                            } />
-                </View>
-                </View>     
+                    <View style={styles.container}>
+                        <View style={{ flex: 1 }}>
+                            <SearchHeader
+                                searchValue={this.state.searchValue}
+                                onClose={this.onClose}
+                                onClear={this.onClear}
+                                placeholder="Search here"
+                                onSearchTextChange={(text) => this.onSearchTextChange(text)}
+                            />
+                            <FlatList
+                                extraData={this.state}
+                                data={this.state.data}
+                                keyExtractor={(item, index) => index.toString()}
+                                renderItem={({ item, index }) =>
+                                    <TechnicianList
+                                        key={index}
+                                        data={item}
+                                        assignJobs={this.alertBoxMessage}
+                                    />
+                                } />
+                        </View>
+                    </View>
                 </Modal>
             </View>
         );
--- yusatateam/app/screens/Jobs/jobAssign/styles.js
+++ yusatateam/app/screens/Jobs/jobAssign/styles.js
@@ -1,68 +1,9 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
-import colors from '../../../constants/colors';
-export default EStyleSheet.create({
-    $headerColor : '#1f667e',
-    $headerIconColor : '#fff',
-    $headerTitleSize : '1.2rem',
-    $headerTitleColor : '#fff',
+import colors from  '../../../constants/colors';
 
+export default EStyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#efefef'
-    },
-    viewList: {
-        flex: 1,
-        flexDirection : 'row',
-        marginLeft :6,
-        marginRight :6,
-        padding: 10
-    },
-    sub_view: {
-        flexDirection: 'row'
-    },
-    jobNumText: {
-        fontSize: '1rem',
-        color: '#000',
-        fontWeight: '500'
-    },
-    right_sub_view: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    jobTypeView: {
-        borderRadius: 10,
-        borderWidth: 0.8,
-        borderColor: 'gray',
-        padding: 2,
-        width: 80,
-        height: 23,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    jobTypeText: {
-        color: '#F98866',
-        fontSize: '0.8rem'
-    },
-    left_view: {
-        flex: 1,
-    },
-    text: {
-        fontSize: '0.9rem',
-        color: '#000',
-    },
-    middle_view: {
-        flex: 0.2,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    right_view: {
-        flex: 1.5,
-    },
-    text1: {
-        fontSize: '0.9rem',
-        color: 'gray'
-    },
-    
+        backgroundColor: '#fff' 
+    }
 });
