ae37fec49a58508ed7beafb18ec2402d1011ed06
shaili <shaili.mittal@yusata.com>
2018-12-03 16:16:26 +0530

    no message

---
+++ yusatateam/app/assets/JSONData/VehicleDetail.js
@@ -0,0 +1,52 @@
+const result=[
+    {
+        vehicleno:'jp85670',
+        Sim :'6',
+        Device : '4',
+        ActivatedDate : '26/04/1995',
+        status : 'Activate'
+    },
+    {
+        vehicleno:'Hl85670',
+        Sim :'6',
+        Device : '4',
+        LastActivated : '26/04/1995',
+        status : 'De-activate'
+    },
+    {
+        vehicleno:'jp85670',
+        Sim :'6',
+        Device : '4',
+        ActivatedDate : '26/04/1995',
+        status : 'Activate'
+    },
+    {
+        vehicleno:'Hl85670',
+        Sim :'6',
+        Device : '4',
+        LastActivated : '26/04/1995',
+        status : 'De-activate'
+    },
+    {
+        vehicleno:'jp85670',
+        Sim :'6',
+        Device : '4',
+        ActivatedDate : '26/04/1995',
+        status : 'Activate'
+    },
+    {
+        vehicleno:'Hl85670',
+        Sim :'6',
+        Device : '4',
+        LastActivated : '26/04/1995',
+        status : 'De-activate'
+    },
+    {
+        vehicleno:'jp85670',
+        Sim :'6',
+        Device : '4',
+        ActivatedDate : '26/04/1995',
+        status : 'Activate'
+    },
+];
+module.exports = result;
