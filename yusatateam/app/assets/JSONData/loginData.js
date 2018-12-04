91d18a39a7a0b0841eced0c9236705c989f6669e
Prem <premsagar.psc@gmail.com>
2018-12-04 14:58:05 +0530

    no message

---
+++ yusatateam/app/assets/JSONData/loginData.js
@@ -0,0 +1,10 @@
+const result= {
+    firstName: 'Premsagar',
+    lastName: 'Choudhary',
+    age: '23',
+    contactNumber: '+918857586502',
+    language: 'English',
+    email: 'premsagar.choudhary@yusata.com',
+}
+
+module.exports = result;
