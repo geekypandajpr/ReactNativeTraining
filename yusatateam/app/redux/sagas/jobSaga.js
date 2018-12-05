2cdfbfa4d35ad052d62db72029cd10ee4e050113
vinayak sharma <sharmavinayak17@hotmail.com>
2018-12-05 16:21:39 +0530

    no message

---
+++ yusatateam/app/redux/sagas/jobSaga.js
@@ -0,0 +1,14 @@
+import { call, put } from 'redux-saga/effects';
+import userService from '../services/userServices';
+import {JOBS} from '../common/actionTypes';
+
+export function* jobPendingData() {
+    try {
+        const data = yield call(userService.jobPendingData );
+        yield put({type: JOBS.JOBS_SUCCESS,data});
+       
+    } catch (error) {
+        yield put({type: JOBS.JOBS_FAILED,error});
+       
+    }
+}
--- yusatateam/app/redux/sagas/rootSaga.js
+++ yusatateam/app/redux/sagas/rootSaga.js
@@ -1,10 +1,12 @@
 import { takeLatest } from 'redux-saga/effects';
 
-import { USER } from '../common/actionTypes';
+import { USER,JOBS} from '../common/actionTypes';
 import * as userSaga from './userSaga';
 import * as simSaga from './simSaga';
+import * as jobSaga from'./jobSaga';
 
 export default function* rootSaga() {
     yield takeLatest(USER.LOGIN, userSaga.login),
-    yield takeLatest(USER.SIM_lOGIN,simSaga.simlogin)
+    yield takeLatest(USER.SIM_lOGIN,simSaga.simlogin),
+    yield takeLatest(JOBS.JOBS_LOGIN,jobSaga.jobPendingData)
 }
