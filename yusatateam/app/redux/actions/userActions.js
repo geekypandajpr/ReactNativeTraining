5ecbdca9321e4ee1bcb1f501a511342fc784442d
vinayak sharma <sharmavinayak17@hotmail.com>
2018-12-05 16:14:49 +0530

    no message

--- yusatateam/app/redux/actions/userActions.js
+++ yusatateam/app/redux/actions/userActions.js
@@ -1,6 +1,7 @@
-import { USER } from '../common/actionTypes';
+import { USER,JOBS } from '../common/actionTypes';
 
 export default {
     loginRequest: (loginCredentials) => ({ type: USER.LOGIN, loginCredentials }) ,
-    simRequest : () =>({ type: USER.SIM_lOGIN })
+    simRequest : () =>({ type: USER.SIM_lOGIN }),
+    jobRequest : () => ({type : JOBS.JOBS_LOGIN})
 }
