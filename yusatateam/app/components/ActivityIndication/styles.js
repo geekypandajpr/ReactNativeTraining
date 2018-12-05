b725c653278e58ab41f67fd8b071a8940dd737ed
Prem <premsagar.psc@gmail.com>
2018-12-05 14:37:33 +0530

    no message

--- yusatateam/app/components/ActivityIndication/styles.js
+++ yusatateam/app/components/ActivityIndication/styles.js
@@ -1,12 +1,14 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { Dimensions } from 'react-native';
 export default EStyleSheet.create({
     container:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        // justifyContent: 'center',
         backgroundColor: 'transparent'
     },
     outer_circle: {
+        marginTop: Dimensions.get('window').height/3,
         borderRadius: 21,
         width: 42,
         height: 42,
