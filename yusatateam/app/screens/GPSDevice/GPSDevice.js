45efdbef444d7cdfe98e78b87ec3bf8006e6e71b
Prem <premsagar.psc@gmail.com>
2018-12-11 15:18:56 +0530

    no message

--- yusatateam/app/screens/GPSDevice/GPSDevice.js
+++ yusatateam/app/screens/GPSDevice/GPSDevice.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export class GPSDevice extends React.Component {
+export default class GPSDevice extends React.Component {
     render() {
         return (
             <View></View>
--- yusatateam/app/screens/GPSDevice/GPSDeviceForm/GPSDeviceForm.js
+++ yusatateam/app/screens/GPSDevice/GPSDeviceForm/GPSDeviceForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export class GPSDeviceForm extends React.Component {
+export default class GPSDeviceForm extends React.Component {
     render() {
         return (
             <View></View>
