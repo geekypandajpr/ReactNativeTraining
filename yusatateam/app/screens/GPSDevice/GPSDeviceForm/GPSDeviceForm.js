601566726e2ede10da14cb4db66a61b7353d0613
shaili <shaili.mittal@yusata.com>
2018-12-12 10:44:26 +0530

    no message

--- yusatateam/app/screens/GPSDevice/GPSDeviceForm/GPSDeviceForm.js
+++ yusatateam/app/screens/GPSDevice/GPSDeviceForm/GPSDeviceForm.js
@@ -1,10 +1,61 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { Textarea, Form, Input, Item } from 'native-base';
+import { Toolbar,Pickers } from '../../../components';
+import styles from './styles';
+import { globalStyles } from '../../../styles'
 
 export default class GPSDeviceForm extends React.Component {
     render() {
         return (
-            <View></View>
+            <View style={{ flex: 1, backgroundColor: '#FFF' }}>
+                <Toolbar title='Dashboard'
+                    leftIcon='home'
+                    setting='md-settings' settingType='Ionicons' onSettingsPress={() => navigate('Settings')} />
+                <View style={{ flex: 1, padding: 5 }}>
+                    <View style={styles.View_row}>
+                        <View style={styles.flex_one}>
+                            <Text style={[globalStyles.primary_text, { fontFamily: 'Roboto' }]}>Device</Text>
+                        </View>
+                        <View style={styles.colon}>
+                            <Text style={[globalStyles.secondary_text, { fontFamily: 'Roboto' }]}>:</Text>
+                        </View>
+                        <View style={styles.flex_two}>
+                            <Form style={{ width: "85%" }}>
+                                <Textarea rowSpan={1} bordered placeholder="Textarea" />
+                            </Form>
+                        </View>
+                    </View>
+
+                    <View style={styles.View_row}>
+                        <View style={styles.flex_one}>
+                            <Text style={[globalStyles.primary_text, { fontFamily: 'Roboto' }]}>Company</Text>
+                        </View>
+                        <View style={styles.colon}>
+                            <Text style={[globalStyles.secondary_text, { fontFamily: 'Roboto' }]}>:</Text>
+                        </View>
+                        <View style={styles.flex_two}>
+                            <Pickers
+                            label1 = "Sim"
+                            label2 = "Sim"
+                            label3 = "Sim"
+                            label4 = "Sim"
+                            label5 = "Sim"
+                            value1 = "key0"
+                            value2 = "key1"
+                            value3 = "key2"
+                            value4 = "key3"
+                            value5 = "key4"
+
+                            >
+                                
+                            </Pickers>
+                        </View>
+                    </View>
+
+
+                </View>
+            </View>
         )
     }
 }
---
+++ yusatateam/app/screens/GPSDevice/GPSDeviceForm/styles.js
@@ -0,0 +1,29 @@
+import EStyleSheet from 'react-native-extended-stylesheet'
+export default EStyleSheet.create({
+
+    flex_one: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+   
+    colon: {
+        flex: 0.2,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+    },
+    flex_two: {
+        flex: 2,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width : '80%'
+    },
+    View_row: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    
+})
