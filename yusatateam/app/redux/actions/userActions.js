86e53ac28eaa491e02ed1a38ffb8722e001b5beb
Prem <premsagar.psc@gmail.com>
2018-12-21 10:53:55 +0530

    no message

---
+++ yusatateam/app/redux/actions/userActions.js
@@ -0,0 +1,14 @@
+import { USER, JOBS, SIM, DEVICE, CUSTOMER, VEHICLE, TECHNICIAN,GPSDEVICE,GPSDEVICECOUNTRYISD } from '../common/actionTypes';
+
+export default {
+    loginRequest: (loginCredentials) => ({ type: USER.LOGIN, loginCredentials }),
+    simRequest: () => ({ type: SIM.SIM_REQUEST }),
+    deviceRequest: () => ({ type: DEVICE.DEVICE_REQUEST }),
+    jobRequest: () => ({ type: JOBS.JOBS_LOGIN }),
+    cutomerFetchRequest: () => ({ type: CUSTOMER.CUSTOMER_FETCH }),
+    vehicleFetchRequest: (customer) => ({ type: VEHICLE.VEHICLE_FETCH, customer }),
+    technicianRequest: () => ({ type: TECHNICIAN.TECHNICIAN_REQUEST }),
+    gpsdeviceRequest: () => ({type: GPSDEVICE.GPSDEVICE_REQUEST}),
+    gpsdeviceRequestr: () => ({ type : GPSDEVICECOUNTRYISD.GPSDEVICECOUNTRYISD_REQUEST })
+
+}
