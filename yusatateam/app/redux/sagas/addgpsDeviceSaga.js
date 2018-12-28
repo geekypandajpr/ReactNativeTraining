0c83bc0e1e4edf6ef52b8fa91b888e87edf84db7
shaili <shaili.mittal@yusata.com>
2018-12-28 17:05:08 +0530

    no message

---
+++ yusatateam/app/redux/sagas/addgpsDeviceSaga.js
@@ -0,0 +1,13 @@
+import {all,put} from 'redux-saga/effects'
+import { ADDGPSVEHICLE } from '../common/actionTypes';
+import userServices from '../services/userServices';
+
+export function* addgpsDeviceLogin(action){
+    try{
+        const data = yield call(userServices.addgpsVehicle);
+        yield put(ADDGPSVEHICLE.ADDGPSVEHICLE_SUCCESS,data)
+    }catch(error){
+        yield put(ADDGPSVEHICLE.ADDGPSVEHICLE_FAILED,error)
+    }
+    
+}
