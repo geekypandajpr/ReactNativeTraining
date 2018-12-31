aa932335801c2d057f71d894b4ced0f6675be165
shaili <shaili.mittal@yusata.com>
2018-12-31 13:50:38 +0530

    no message

---
+++ yusatateam/app/redux/reducers/GPSDevice/createVehicleTypeReducer.js
@@ -0,0 +1,33 @@
+import { GPSDEVICE } from '../../common/actionTypes'
+
+const initialState = {
+    isLoading: false,
+    data: [],
+    isFetched: false,
+    error: false
+}
+
+export default createVehicleTypeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GPSDEVICE.CREATE_VEHICLETYPE_REQUEST:
+            return Object.assign({}, state, {
+                isLoading: true,
+                isFetched: false
+            });
+        case GPSDEVICE.CREATE_VEHICLETYPE_SUCCESS:
+            return Object.assign({}, state, {
+                data: action.data,
+                isLoading: false,
+                isFetched: true
+            });
+        case GPSDEVICE.GPSDEVICEVEHICLELIST_REQUEST:
+            return Object.assign({}, state, {
+                isLoading: false,
+                error: true,
+                isFetched: false,
+                data: [],
+            });
+        default:
+            return state;
+    }
+}
