95fe6a74ad71795a9003017c4a954578983ee5b9
vinayak sharma <sharmavinayak17@hotmail.com>
2019-01-03 16:03:02 +0530

    no message

--- yusatateam/app/screens/QrCodeScanner/QrCode.js
+++ yusatateam/app/screens/QrCodeScanner/QrCode.js
@@ -3,120 +3,150 @@ import { Alert, Linking, Dimensions, LayoutAnimation, Text, View, StatusBar, Tou
 import { BarCodeScanner, Permissions } from 'expo';
 import { Toolbar, SearchBar } from '../../components';
 import styles from './styles';
+import { userActions } from '../../redux/actions';
+import { connect } from 'react-redux';
+import { DeviceInfo} from './DeviceInfo'
 
-export default class QrCode extends Component {
-  state = {
-    hasCameraPermission: null,
-    lastScannedUrl: null,
-    deviceUDID: '',
-    bottomBar : false
-  };
+export  class QrCode extends Component {
+    state = {
+        hasCameraPermission: null,
+        lastScannedUrl: null,
+        deviceUDID: '',
+        bottomBar: false,
+        deviceInfoData: {},
+        isGetDeviceUDID: false,
+    };
 
-  componentDidMount() {
-    this._requestCameraPermission();
-  }
-
-  _requestCameraPermission = async () => {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted',
-    });
-  };
-
-  _handleBarCodeRead = result => {
-    if (result.data !== this.state.lastScannedUrl) {
-      LayoutAnimation.spring();
-      this.setState({ lastScannedUrl: result.data,bottomBar : true});
+    componentDidMount() {
+        this._requestCameraPermission();
     }
-  };
 
-  getDeviceInfo(value) {
-    alert("You have search "+value);
-}
+    _requestCameraPermission = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({
+            hasCameraPermission: status === 'granted',
+        });
+    };
 
-  render() {
-    const { goBack } = this.props.navigation;
-    return (
-      <View style={styles.container}>
-      <Toolbar title='Device Report'
-        leftIcon='arrow-left' leftIconType='Feather' onLeftButtonPress={() => goBack()}/>
-      <View style={{flex :1,justifyContent:'center',alignItems:'center'}}>
-      <SearchBar
-        placeholder={'Search by device ESN'}
-        isDropdown={false}
-        onChangeText={(text) => this.setState({lastScannedUrl: text})}
-        value={this.state.lastScannedUrl}
-        onSearch={() => this.getDeviceInfo(this.state.lastScannedUrl)}
+    _handleBarCodeRead = result => {
+        if (result.data !== this.state.lastScannedUrl) {
+            LayoutAnimation.spring();
+            this.setState({ lastScannedUrl: result.data, bottomBar: true });
+        }
+    };
+
+    getDeviceInfo() {
+        this.setState({ isSearching: true });
+        this.props.fetchDeviceInfo(this.state.lastScannedUrl);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.deviceInfo.isFetched && this.state.isSearching) {
+            //alert(JSON.stringify(nextProps.deviceInfo.deviceInfo))
+            this.setState({deviceInfoData : nextProps.deviceInfo.deviceInfo, isGetDeviceUDID: true})
+        }        
+    }
+
+    render() {
+        const { goBack } = this.props.navigation;
+        return (
+            <View style={styles.container}>
+                <Toolbar title='Device Report'
+                    leftIcon='arrow-left' leftIconType='Feather' onLeftButtonPress={() => goBack()} />
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <SearchBar
+                        placeholder={'Search by device ESN'}
+                        isDropdown={false}
+                        onChangeText={(text) => this.setState({ lastScannedUrl: text })}
+                        value={this.state.lastScannedUrl}
+                        onSearch={() => this.getDeviceInfo()}
                     />
-      </View>
-        <View style={{flex :9}}>
-        {this.state.hasCameraPermission === null
-                ? <Text>Requesting for camera permission</Text>
-                : this.state.hasCameraPermission === false
-                    ? <Text style={{ color: '#fff' }}>
-                        Camera permission is not granted
-                        </Text>
-                    : 
-                    <View style={{flex :1,justifyContent:'center',alignItems:'center'}}>
-                    <BarCodeScanner
-                        onBarCodeRead={this._handleBarCodeRead}
-                        style={styles.barCode} /></View>
+                </View>
+                { this.state.isGetDeviceUDID ?
+                    <DeviceInfo onClose={() => this.setState({ isGetDeviceUDID: false, isSearching: false, lastScannedUrl: '' })}
+                        deviceUDID={this.state.lastScannedUrl}
+                        deviceInfoData={this.state.deviceInfoData}
+                    />
+                    :
+                    <View style={{ flex: 9 }}>
+                        {this.state.hasCameraPermission === null
+                            ? <Text>Requesting for camera permission</Text>
+                            : this.state.hasCameraPermission === false
+                                ? <Text style={{ color: '#fff' }}>
+                                    Camera permission is not granted
+                                            </Text>
+                                :
+                                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                                    <BarCodeScanner
+                                        onBarCodeRead={this._handleBarCodeRead}
+                                        style={styles.barCode} /></View>
                         }
 
-                {this._maybeRenderUrl()}
+                        {this._maybeRenderUrl()}
 
-                {/* <StatusBar hidden /> */}
-        </View>
-       
-      </View>
-    );
-  }
-
-  _handlePressUrl = () => {
-    Alert.alert(
-      'Open this URL?',
-      this.state.lastScannedUrl,
-      [
-        {
-          text: 'Yes',
-          onPress: () => Linking.openURL(this.state.lastScannedUrl),
-        },
-        { text: 'No', onPress: () => {} },
-      ],
-      { cancellable: false }
-    );
-  };
-
-  _handlePressCancel = () => {
-    this.setState({ bottomBar : false});
-  };
-
-  _maybeRenderUrl = () => {
-    if (!this.state.lastScannedUrl) {
-      return;
+                        {/* <StatusBar hidden /> */}
+                    </View>
+                }
+            </View>
+        );
     }
 
-    return (
-        <View>
-            { this.state.bottomBar ?  <View style={styles.bottomBar}>
-            <TouchableOpacity style={styles.url} onPress={this._handlePressUrl}>
-            <Text numberOfLines={1} style={styles.urlText}>
-                {this.state.lastScannedUrl}
+    _handlePressUrl = () => {
+        Alert.alert(
+            'Open this URL?',
+            this.state.lastScannedUrl,
+            [
+                {
+                    text: 'Yes',
+                    onPress: () => Linking.openURL(this.state.lastScannedUrl),
+                },
+                { text: 'No', onPress: () => { } },
+            ],
+            { cancellable: false }
+        );
+    };
+
+    _handlePressCancel = () => {
+        this.setState({ bottomBar: false });
+    };
+
+    _maybeRenderUrl = () => {
+        if (!this.state.lastScannedUrl) {
+            return;
+        }
+
+        return (
+            <View>
+                {this.state.bottomBar ? <View style={styles.bottomBar}>
+                    <TouchableOpacity style={styles.url} onPress={this._handlePressUrl}>
+                        <Text numberOfLines={1} style={styles.urlText}>
+                            {this.state.lastScannedUrl}
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.cancelButton}
+                        onPress={this._handlePressCancel}>
+                        <Text style={styles.cancelButtonText}>
+                            Ok
             </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-            style={styles.cancelButton}
-            onPress={this._handlePressCancel}>
-            <Text style={styles.cancelButtonText}>
-                Ok
-            </Text>
-            </TouchableOpacity>
-        </View> : null }
-           
-   </View>
-    );
-  };
+                    </TouchableOpacity>
+                </View> : null}
+
+            </View>
+        );
+    };
 }
 
+function mapStateToProps(state) {
+    return {
+        deviceInfo: state.deviceInfo
+    }
+}
 
-export {QrCode}
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchDeviceInfo: (deviceUDID) => dispatch(userActions.getAssociationDeviceInfo(deviceUDID))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QrCode);
