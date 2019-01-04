3e4164dec881553e93a8d4360f3fedc9c7083fd4
shaili <shaili.mittal@yusata.com>
2019-01-04 17:04:50 +0530

    no message

--- yusatateam/app/screens/GPSDevice/GPSDeviceForm/CreateVehicle.js
+++ yusatateam/app/screens/GPSDevice/GPSDeviceForm/CreateVehicle.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Image } from 'react-native';
-import { Item, Input, Icon, Card, Button, Text} from 'native-base';
-import { ImagePicker } from 'expo';
+import { Item, Input, Icon, Card, Button, Text } from 'native-base';
+import { ImagePicker,Permissions } from 'expo';
 import styles from './styles';
 import { Toolbar } from '../../../components';
-import {BarCodeModal} from './BarCodeModal';
+import { BarCodeModal } from './BarCodeModal';
 
 export default class CreateVehicle extends React.Component {
     constructor(props) {
@@ -13,33 +13,39 @@ export default class CreateVehicle extends React.Component {
             data: null,
             searchValue: '',
             image: null,
-            deviceId : null,
-            vim : null,
-            sim :null,
-            barValueGet : new Map(),
+            hasCameraPermission: null,
+            cameraperm: null,
+            deviceId: null,
+            vim: null,
+            sim: null,
+            barValueGet: new Map(),
         };
         this.modalReference = React.createRef();
         this.BarCodePage = this.BarCodePage.bind(this);
     }
 
+    async componentDidMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ cameraperm: status === 'granted' });
+        const { cstatus } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        this.setState({ hasCameraPermission: cstatus === 'granted' });
+    }
+
     barCodeValue(value) {
-        
-        if(value.get("Device"))
-        {
-             this.setState({deviceId : value.get("Device") })
+
+        if (value.get("Device")) {
+            this.setState({ deviceId: value.get("Device") })
         }
-        if(value.get("VIN"))
-        {
-             this.setState({vim : value.get("VIN") })
+        if (value.get("VIN")) {
+            this.setState({ vim: value.get("VIN") })
         }
-        if(value.get("SIM"))
-        {
-             this.setState({sim : value.get("SIM") })
+        if (value.get("SIM")) {
+            this.setState({ sim: value.get("SIM") })
         }
-     }
-     BarCodePage(data) {
-         this.modalReference.current.setModalVisible(true,data);
-     }
+    }
+    BarCodePage(data) {
+        this.modalReference.current.setModalVisible(true, data);
+    }
 
     _pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -50,6 +56,16 @@ export default class CreateVehicle extends React.Component {
             this.setState({
                 image: result.uri
             })
+        }
+    }
+    _takephoto = async () => {
+        let pickResult = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [4, 3],
+        });
+
+        if (!pickResult.cancelled) {
+            this.setState({ image: pickResult.uri });
         }
     }
 
@@ -72,10 +88,10 @@ export default class CreateVehicle extends React.Component {
                             {/* <Icon name='barcode' /> */}
                         </Item>
                         <Item style={styles.InputItem}>
-                                <Input placeholder='Device Id'
-                                value={this.state.deviceId}/>
-                                <Icon name='barcode'  onPress={() => this.BarCodePage("Device")}/>
-                            </Item>
+                            <Input placeholder='Device Id'
+                                value={this.state.deviceId} />
+                            <Icon name='barcode' onPress={() => this.BarCodePage("Device")} />
+                        </Item>
                         <Item style={styles.InputItem}>
                             <Input placeholder='Department Id' />
                             {/* <Icon name='barcode' /> */}
@@ -85,30 +101,30 @@ export default class CreateVehicle extends React.Component {
                             {/* <Icon name='barcode' /> */}
                         </Item>
                         <Item style={styles.InputItem}>
-                                <Input placeholder='Sim number'
-                                value={this.state.sim}/>
-                                <Icon name='barcode' onPress={() => this.BarCodePage("SIM")} />
-                            </Item>
-                            <Item style={styles.InputItem}>
-                                <Input placeholder='VIN'
-                                value={this.state.vim}/>
-                                <Icon name='barcode' onPress={() => this.BarCodePage("VIN")} />
-                            </Item>
+                            <Input placeholder='Sim number'
+                                value={this.state.sim} />
+                            <Icon name='barcode' onPress={() => this.BarCodePage("SIM")} />
+                        </Item>
+                        <Item style={styles.InputItem}>
+                            <Input placeholder='VIN'
+                                value={this.state.vim} />
+                            <Icon name='barcode' onPress={() => this.BarCodePage("VIN")} />
+                        </Item>
                     </View>
 
                     <View style={{ flex: 2, flexDirection: 'row', marginTop: "10%" }}>
 
                         <View style={{ flex: 0.3, alignItems: 'center', justifyContent: 'center', marginTop: '1%' }}>
                             <View style={{ height: 90, width: 90, borderColor: '#000', borderWidth: 1, borderRadius: 45, alignItems: 'center', justifyContent: 'center' }}>
-                                <Image source={{ uri: image }}
-                                    style={{ alignItems: 'center', justifyContent: 'center' }} />
+                                <Image source={{ uri: image }} resizeMode='cover'
+                                    style={{ alignItems: 'center', justifyContent: 'center', height: 100, width: 100, borderRadius: 50 }} />
                             </View>
                         </View>
 
                         <View style={{ flex: 0.6, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '1%' }}>
                             <View style={{ alignItems: 'center', justifyContent: 'center' }}>
                                 <Button bordered dark style={{ height: 35, borderColor: 'gray' }}
-                                    onPress={() => alert('capture Image')}>
+                                    onPress={this._takephoto}>
                                     <Text style={styles.createVehicle}>Capture Image</Text>
                                 </Button>
                             </View>
