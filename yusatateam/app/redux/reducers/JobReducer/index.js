9f15cc0f74cf6824b2ab0ac7b1cae96312c83589
vinayak sharma <sharmavinayak17@hotmail.com>
2019-01-10 16:11:52 +0530

    no message

--- yusatateam/app/redux/reducers/JobReducer/index.js
+++ yusatateam/app/redux/reducers/JobReducer/index.js
@@ -4,3 +4,4 @@ export { default as jobListReducer } from './jobListReducer';
 export { default as companyReducer } from './companyReducer';
 export { default as vehicleReducer } from './vehicleReducer';
 export { default as executeServiceReducer } from './executeServiceReducer';
+export { default as serviceStatusReducer } from './serviceStatusReducer';
