2dbed64d8e6cb8c4050553207feb8c6d63782f2e
Prem <premsagar.psc@gmail.com>
2019-01-10 14:58:24 +0530

    no message

--- yusatateam/app/redux/reducers/JOBREDUCER/index.js
+++
@@ -1,5 +0,0 @@
-export { default as createJobReducer } from './createJobReducer';
-export { default as jobHistoryReducer } from './jobHistoryReducer';
-export { default as jobListReducer } from './jobListReducer';
-export { default as companyReducer } from './companyReducer';
-export { default as vehicleReducer } from './vehicleReducer';
