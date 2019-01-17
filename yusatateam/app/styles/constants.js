bb26205febd5dbde8a993e584a20c12874593cd2
Prem <premsagar.psc@gmail.com>
2019-01-17 13:43:50 +0530

    no message

--- yusatateam/app/styles/constants.js
+++ yusatateam/app/styles/constants.js
@@ -1,6 +1,6 @@
 export const typeCode = {
     /**<----------------------------SERVICE TYPE CODE-----------------------------> */
-    SERVICE_ORDER_CODE : 'TPI_SERVICE',
-    DEVICE_ORDER_CODE : 'TPI_DEVICE',
-    SIM_ORDER_CODE : 'TPI_SIM',
+    SERVICE_ORDER_CODE          : 'TPI_SERVICE',
+    DEVICE_ORDER_CODE           : 'TPI_DEVICE',
+    SIM_ORDER_CODE              : 'TPI_SIM',
 }
